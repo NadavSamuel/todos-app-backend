85860d7ff631c9bfcb23efc7e5c7f2a1f2287def
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-06 12:27:28 +0200

    database var fix

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority",
+  "dbURL": "mongodb+srv://Nadav:Y6a558nfNjfzxgNo@cluster0.h668w.mongodb.net/todo_db?retryWrites=true&w=majority",
 }
