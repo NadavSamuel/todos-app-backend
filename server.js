d8be7e9ad43c81b4e29c702462f5fc56a1e07999
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-07 11:50:07 +0200

    cors fix 6

--- server.js
+++ server.js
@@ -26,28 +26,28 @@ app.use(session({
 // });
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')));
-// } else {
-//     const corsOptions = {
-//         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','https://todos-next-js.vercel.app/'],
-//         credentials: true
-//     };
-//     app.use(cors(corsOptions));
-// }
-var corsOptions = {
-    origin: '*',
-    credentials : true
-   }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')));
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','https://todos-next-js.vercel.app/'],
+        credentials: true
+    };
+    app.use(cors(corsOptions));
+}
+// var corsOptions = {
+//     origin: '*',
+//     credentials : true
+//    }
 
-app.use(cors(corsOptions))
-app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', '*');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
-    res.setHeader('Access-Control-Allow-Credentials', true);    
-    next();
-});
+// app.use(cors(corsOptions))
+// app.use(function (req, res, next) {	
+//     res.setHeader('Access-Control-Allow-Origin', '*');    
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+//     res.setHeader('Access-Control-Allow-Credentials', true);    
+//     next();
+// });
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
