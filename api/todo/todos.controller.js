d6f26c806c322ce4bfe778c7bd83282be3dc7d26
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-08 15:41:12 +0200

    session console.log added

--- api/todo/todos.controller.js
+++ api/todo/todos.controller.js
@@ -3,6 +3,9 @@ const todoService = require('./todo.service')
 
 async function getTodos(req, res) {
     try {
+        console.log('req.session, ',req.session)
+        logger.info('req.session', req.session);
+
         const query = {byUser:req.session.user.username,...req.query}
         const todos = await todoService.query(query)
         res.send(todos)
