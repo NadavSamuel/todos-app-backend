eb74fd1fc93f2f84649fe6b017af84b7fa3c7bee
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-08 16:10:52 +0200

    session fix 1

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.use(session({
         credentials: true
     };
     app.use(cors(corsOptions));
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        next();
+    });
 // var corsOptions = {
 //     origin: '*',
 //     credentials : true
