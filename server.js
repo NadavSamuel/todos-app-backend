71f76340eca53ca3f9f65e0e924904452dfe07e6
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-09 12:52:28 +0200

    session fix 8

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(session({
     cookie: { secure: false,httpOnly: false}
 }))
 
-
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','https://todos-next-js.vercel.app'],
         credentials: true
