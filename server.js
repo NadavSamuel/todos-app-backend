79a5f3da13cc9f3f3b10c23286ed34d62364696c
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-11 10:55:04 +0200

    sess cookie fix 24

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    console.log('req.headers.origin, ',req.headers.origin)
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
